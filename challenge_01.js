f82fd9b6e76f8d0cc495d50ef5891ef97f6a4218
Marco <marco_2@gmx.de>
2021-05-18 15:31:51 +0200

    Hausaufgabe Prompt, parseInt, parseFloat

--- challenge_01.js
+++ challenge_01.js
@@ -8,3 +8,23 @@ Berechnen Sie die Summe der Zahlen.
 Geben Sie die Summe der Zahlen in die Konsole aus:
 ‘Die Summe der Zahlen ist: summe ‘
 */
+
+/*
+let stringOne,stringTwo;
+let numberOne,numberTwo;
+stringOne = prompt("Geben Sie eine Zahl ein");
+numberOne = parseInt (stringOne);
+stringTwo = prompt("Geben Sie eine weitere Zahl ein");
+numberTwo = parseInt (stringTwo)
+let result = (numberOne + numberTwo);
+
+console.log("Die Summer der Zahlen ist: " + result);
+*/
+let numberOne,numberTwo;
+numberOne = prompt("Geben Sie eine Zahl ein");
+//numberOne = parseInt (numberOne);
+numberTwo = prompt("Geben Sie eine weitere Zahl ein");
+//numberTwo = parseInt (numberTwo)
+let result = (parseFloat(numberOne) + parseFloat(numberTwo));
+console.log("Die Summer der Zahlen ist: " + result);
+
